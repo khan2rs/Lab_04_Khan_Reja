4001916b7ad7192d99e29acf130df9a7f80f5343
khan2rs <khan2rs@mail.uc.edu>
2022-09-19 23:18:49 -0400

    commit

---
+++ src/SalesTax.java
@@ -0,0 +1,9 @@
+public class SalesTax {
+    public static void main(String[] args) {
+
+        System.out.println("What is the price of your purchase?");
+        double purchaseAmount = 50;
+        double salesTax = purchaseAmount * .05;
+                System.out.println("Your sales tax is " + salesTax);
+    }
+}
